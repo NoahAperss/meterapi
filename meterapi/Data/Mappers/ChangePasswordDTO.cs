e0001b09d0b708cf1d6f43df973115c49700e316
Noah Apers <79966428+NoahAperss@users.noreply.github.com>
2023-01-27 09:19:53 +0100

    changepass change email werkend

---
+++ meterapi/Data/Mappers/ChangePasswordDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace meterapi.Data.Mappers
+{
+    public class ChangePasswordDTO
+    {
+        public string newPassword { get; set; }
+        public string oldPassword { get; set; }
+
+        public string email { get; set; }
+    }
+}
