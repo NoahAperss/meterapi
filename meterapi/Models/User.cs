470adf2132ab4c2a518117e40e6e706ec4f73581
Noah Apers <79966428+NoahAperss@users.noreply.github.com>
2023-01-31 10:58:36 +0100

    fixFirstLogin

--- meterapi/Models/User.cs
+++ meterapi/Models/User.cs
@@ -14,11 +14,17 @@ namespace meterapi.Models
         public string RefreshToken { get; set; } = string.Empty;
         public DateTime TokenCreated { get; set; }
         public DateTime TokenExpires { get; set; }
+        public bool IsFirstLogin { get; set; } = false;
 
         public virtual ICollection<UserMeter> UserMeters { get; set; }
         public string? VerificationToken { get; internal set; }
-           public bool IsEmailVerified { get; set; } = false;
+        public bool IsEmailVerified { get; set; } = false;
         public string? ResetPasswordToken { get; internal set; }
-        public DateTime? ResetPasswordExpiration { get; internal set; }
+        public DateTime? ResetPasswordExpiration
+        {
+            get; internal set;
+
+
+        }
     }
 }
